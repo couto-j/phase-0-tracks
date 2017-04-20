fdc274be77368650b29534fedbea0398ae3f97f0
Jeffrey Couto <jeffrey.couto@gmail.com>
2017-04-20 14:13:52 -0500

    Created first set of questions to ask employees of Werewolf, Inc.

---
+++ ruby/vampires.rb
@@ -0,0 +1,29 @@
+puts "What is your name?"
+
+your_name = gets
+your_name = your_name.chomp
+
+puts "How old are you? What year were you born?"
+
+age = gets
+age = age.chomp
+
+d_o_b = gets
+d_o_b = d_o_b.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+bread = gets
+bread = bread.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+insurance = gets
+insurance = insurance.chomp
+
+
+
+
+
+
+
