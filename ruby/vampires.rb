dcb820fc5d6a5a3546be6434d8363bbce7d98f1e
Jeffrey Couto <jeffrey.couto@gmail.com>
2017-04-21 17:09:12 -0500

    Added check for sunshine allergy.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,57 +13,78 @@ counter = 0
 until counter == number_employees
 
 # Four introductory questions.	
-puts "What is your name?"
-name = gets.chomp
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "How old are you?"
-age = gets.chomp.to_i
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
-puts "What year where you born?"
-birth_year = gets.chomp.to_i
+	puts "What year where you born?"
+	birth_year = gets.chomp.to_i
 
-# Checking for age versus birth year
+	# Checking for age versus birth year
 
-this_year = Time.new.year
-	if (this_year - birth_year) == age
-		age_check = true
-	else
-		age_check = false
+	this_year = Time.new.year
+		if (this_year - birth_year) == age
+			age_check = true
+		else
+			age_check = false
+		end
+
+	puts "Should we order some garlic bread for you?"
+	bread = gets.chomp.downcase
+		if bread == "yes"
+			order_bread = true
+		else 
+			order_bread = false
+		end 
+
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp.downcase
+		if insurance == "yes"
+			buy_insurance = true
+		else
+			buy_insurance = false
+		end
+
+#Checking for allergies.
+
+	puts "Do you have any allergies to list?"
+	allergies = gets.chomp.downcase
+	if allergies == "yes"
+		add_allergies = true
+		while add_allergies
+			puts "Enter your allergies, one at a time. Enter 'done' when you've finished."
+			allergy = gets.chomp.downcase
+			if allergy == "sunshine"
+			result = "Probably a vampire"
+			sunshine = true
+			add_allergies = false
+			elsif allergy == "done"
+			add_allergies = false
+			end
+		end
 	end
 
-puts "Should we order some garlic bread for you?"
-bread = gets.chomp.downcase
-	if bread == "yes"
-		order_bread = true
-	else 
-		order_bread = false
-	end 
-
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp.downcase
-	if insurance == "yes"
-		buy_insurance = true
-	else
-		buy_insurance = false
-	end
 
 #Conditonals to check for vampires.
-
-result = String.new
-	if age_check && (order_bread || buy_insurance)
-		result = "Probably not a vampire."
-	elsif age_check == false && (order_bread == false || buy_insurance == false)
-		result = "Probably a vampire."
+	if sunshine != true
+		result = String.new
+			if age_check && (order_bread || buy_insurance)
+				result = "Probably not a vampire."
+			elsif age_check == false && (order_bread == false || buy_insurance == false)
+				result = "Probably a vampire."
+			end
+			if age_check == false && order_bread == false && buy_insurance == false
+				result = "Almost certainly a vampire."
+			end
+			if name == "Drake Cula" or name == "Tu Fang"
+				result = "Definitely a vampire."
+			end
+			if result == ""
+				result = "Results inconclusive."
+			end
+		puts result 
 	end
-	if age_check == false && order_bread == false && buy_insurance == false
-		result = "Almost certainly a vampire."
-	end
-	if name == "Drake Cula" or name == "Tu Fang"
-		result = "Definitely a vampire."
-	end
-	if result == ""
-		result = "Results inconclusive."
-	end
-puts result 
 end
 
