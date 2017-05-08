3ee51e8320596e2a559ac2f40d1bccee4b3c6e8c
Jeffrey Couto <jeffrey.couto@gmail.com>
2017-05-08 10:35:24 -0500

    Completed Release 1.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,29 @@
 class Puppy
-
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  def speak(time)
+    time.times do |time|
+      puts "Woof!"
+    end 
+  end
+  def roll_over 
+    puts "*rolls over*"
+  end
+  def sit
+    puts "I am sitting down!"
+  end
 
 end
+
+
+# emily = Puppy.new
+# emily.fetch("ball")
+# emily.speak(5)
+# emily.roll_over
+# emily.sit
 
