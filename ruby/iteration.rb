23af54a49152c244e6ff51f6d1b421a45fddcb8a
Jeffrey Couto <jeffrey.couto@gmail.com>
2017-05-22 15:30:48 -0500

    Completed Release 1 with hash.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -34,4 +34,23 @@ end
 puts "After .map! call:"
 p inventory
 
+inventory_count = { "skeins of yarn" => 6, "knitting needles" => 3, "patterns" => 2, "stitch markers" => 3}
+
+puts "After .each on hash:"
+
+inventory_count.each do |item, number|
+  puts "You have #{number} #{item} in your knitting bag."
+end
+
+puts "after .map on hash:"
+updated_inventory = inventory_count.map do |item, number|
+  number = number+1
+  item = item.upcase
+  puts "You now have #{number+1} #{item} in your knitting bag."
+end
+
+puts inventory_count
+p updated_inventory
+
+
 
