f2d816b9a510d7b0bd25ff61639982b6d054616b
Jeffrey Couto <jeffrey.couto@gmail.com>
2017-05-22 15:13:26 -0500

    Completed Release 1 with array.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,4 +6,32 @@ def items
 	yield(item1, item2)
 end
 
-items { |item1, item2| puts "It looks like you're carrying around #{item1} and #{item2}." }
+puts "Here's what you have:"
+items { |item1, item2| puts "You have some #{item1} and #{item2} in your knitting bag."}
+
+
+#Release 1: Using .each, .map, and .map!
+
+inventory = ["skein of yarn", "pair of knitting needles", "pattern", "row counter"]
+
+puts "After .each on array:"
+inventory.each do |item|
+	puts "You have a #{item} in your knitting bag."
+end
+
+modfied_inventory = inventory.map do |item|
+  item.upcase
+end
+
+puts "After .map call:"
+p inventory
+p modfied_inventory
+
+inventory.map! do |item|
+  "nothing to see here!"
+end
+
+puts "After .map! call:"
+p inventory
+
+
