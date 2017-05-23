5ea7d6eef1e40ff62fbf293b0cd7e044db36e7c4
Jeffrey Couto <jeffrey.couto@gmail.com>
2017-05-22 23:28:12 -0500

    Completed iterating through data structures.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,33 +1,36 @@
-puts " First day of class".center(20).upcase
+puts "Knitting Needle Inventory".upcase
 
-school = 
-{
-  first_floor: 
-  {
+inventory = {
   
-  "room 101" => ["maria, nita, manny, bob"],
-  "room 102" => ["james, emily, sophia, dexter"],
-  "room 103" => [ "kia, chastity, tina, tom"],
-
+  circular:{
+    '16 inch' => ['US 2', 'US 4', 'US 6', 'US 8', 'US 10'],
+    '24 inch' => ['US 2', 'US 3', 'US 4', 'US 5', 'US 6', 'US 7'],
+    '32 inch' => ['US 0', 'US 1', 'US 2', 'US 4', 'US 9'],
+    '40 inch' => ['US 1.5', 'US 2', 'US 2.5', 'US 3', 'US 4'],
+    '60 inch' => [],
   },
   
-  second_floor: 
-  {
-  "room 201"=> [ "jenny, tanya, natasha, ashley"],
-  "room 202"=> [ "joey, sue, lindsey, jim"]
+  straight:{
+    '10 inch' => ['US 4', 'US 5', 'US 6', 'US 7', 'US 8', 'US 9'],
+    '14 inch' => ['US 3', 'US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
   }
+  
 }
 
-
-puts school [:first_floor]
-
-puts school [:second_floor]
-
-puts school [:first_floor]["room 101"] 
-
-puts school [:second_floor]["room 202"]
-
-puts school [:second_floor]
-
-puts school [:first_floor]
-puts school [:second_floor]
+puts "Here is the entire current knitting needle inventory:"
+puts inventory [:circular]
+puts "Here are the 16 inch circulars we have in stock:"
+puts inventory[:circular]['16 inch']
+puts "Here are the 24 inch circulars we have in stock:"
+puts inventory[:circular]['24 inch']
+puts "We just got 60 inch circular needles in stock! Here they are:"
+puts inventory [:circular]['60 inch'].push('US 2', 'US 3', 'US 4')
+puts "Here are the straight needles we have in stock:"
+puts inventory[:straight]
+puts "Here are the straight 14 inch needles we have in stock:"
+puts inventory[:straight]['14 inch']
+puts "Here are the straight 10 inch needles we have in stock:"
+puts inventory[:straight]['10 inch']
+puts "Oppsie. Looks like we got one of those wrong. Here's the updated list of 10 inch straight needles:"
+inventory[:straight]['10 inch'][0] = 'US 2'
+puts inventory[:straight]['10 inch']
